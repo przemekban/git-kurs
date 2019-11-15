6962915e43162b51d768df9007aab8a14b1159c1
przemek <przeban3354@wp.pl>
2019-11-15 14:50:47 +0100

    Skok mastera i pozdrowienia

--- src/main/java/pl/mpas/git_examples/Hello.java
+++ src/main/java/pl/mpas/git_examples/Hello.java
@@ -4,5 +4,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello git");
         System.out.println("Pozdrowienia sąsiedzie");
+        System.out.println("Kolejne pozdrowienia lecą mordo!");
     }
 }
