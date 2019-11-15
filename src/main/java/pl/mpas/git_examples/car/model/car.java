5b9333dfdef18bd73100bfd8a13e4f6e32c25191
przemek <przeban3354@wp.pl>
2019-11-15 11:36:40 +0100

    new file car added @Data

---
+++ src/main/java/pl/mpas/git_examples/car/model/car.java
@@ -0,0 +1,16 @@
+package pl.mpas.git_examples.car.model;
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+public class car {
+    private String brand;
+    private String model;
+    private String color;
+    private BigDecimal price;
+    private int engine;
+
+
+}
