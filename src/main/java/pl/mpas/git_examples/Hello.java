fd8410f0bd074d25a96597ca7fc46d8620e5e100
przemek <przeban3354@wp.pl>
2019-11-15 15:00:44 +0100

    [add] - in Hello sout hasta la vista

--- src/main/java/pl/mpas/git_examples/Hello.java
+++ src/main/java/pl/mpas/git_examples/Hello.java
@@ -4,5 +4,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello git");
         System.out.println("Pozdrowienia sąsiedzie");
+        System.out.println("Hasta la vista");
     }
 }
